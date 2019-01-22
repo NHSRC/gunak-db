fbb40869d8fad88934e8cec435513881f9cac1fa
Vivek Singh <petmongrels@gmail.com>
2019-01-22 17:26:31 +0530

    modified assessment view to add all reqd fields for the dashboard

--- Views.sql
+++ Views.sql
@@ -1,34 +1,16 @@
 DROP VIEW if exists recent_facility_assessment_view;
-DROP VIEW if exists checkpoint_scores;
 DROP VIEW if exists checkpoint_scores_aoc;
 drop VIEW IF EXISTS checkpoint_denormalised;
-DROP view if exists checkpoint_score_denormalised;
 DROP view if exists assessment_denormalised;
 DROP VIEW if exists checklist_score_view;
 DROP VIEW if exists area_of_concern_score_view;
 DROP VIEW if exists standard_score_view;
-
-CREATE OR REPLACE VIEW checkpoint_scores AS
-  SELECT
-    assessment_tool_mode.id AS assessment_type,
-    fa.series_name          AS assessment_number,
-    facility.id             AS facility,
-    d.id                    AS department,
-    c.name                  AS checkpoint,
-    cs.score                AS score
-  FROM checkpoint_score cs
-    INNER JOIN checkpoint c ON cs.checkpoint_id = c.id
-    LEFT OUTER JOIN checklist cl ON cl.id = cs.checklist_id
-    LEFT OUTER JOIN department d ON d.id = cl.department_id
-    LEFT OUTER JOIN facility_assessment fa ON cs.facility_assessment_id = fa.id
-    LEFT OUTER JOIN facility ON fa.facility_id = facility.id
-    LEFT OUTER JOIN assessment_tool ON fa.assessment_tool_id = assessment_tool.id
-    LEFT OUTER JOIN assessment_tool_mode ON assessment_tool_mode.id = assessment_tool.assessment_tool_mode_id;
+DROP VIEW if exists standard_score_view;
 
 CREATE or replace VIEW checkpoint_scores_aoc AS
   SELECT
     assessment_tool_mode.id                                  AS assessment_type,
-    fa.series_name                                           AS assessment_number,
+    to_number(fa.series_name, '9999')                   AS assessment_number,
     facility.id                                              AS facility,
     format('%s (%s)', aoc.reference, aoc.name)               AS area_of_concern,
     department.id                                            AS department,
@@ -47,7 +29,10 @@ CREATE or replace VIEW checkpoint_scores_aoc AS
     aoc.id                                                   as area_of_concern_id,
     facility_type.name                                       as faclity_type_name,
     cl.id as checklist_id,
-    cl.name as checklist_name
+    cl.name as checklist_name,
+    me.name as measurable_element_name,
+    me.reference as measurable_element_reference,
+    fa.facility_name as non_coded_facility_name
   FROM checkpoint_score cs
     INNER JOIN checkpoint c ON cs.checkpoint_id = c.id
     LEFT OUTER JOIN checklist cl ON cl.id = cs.checklist_id
@@ -80,43 +65,25 @@ CREATE OR REPLACE VIEW checkpoint_denormalised AS
     INNER JOIN assessment_tool ON checklist.assessment_tool_id = assessment_tool.id
     INNER JOIN assessment_tool_mode ON assessment_tool.assessment_tool_mode_id = assessment_tool_mode.id;
 
-CREATE VIEW checkpoint_score_denormalised AS
-  SELECT
-    checkpoint_score.id     checkpoint_score_id,
-    facility_assessment.id  facility_assessment_id,
-    measurable_element.id   measurable_element_id,
-    standard.id             standard_id,
-    area_of_concern.id      area_of_concern_id,
-    checklist.id            checklist_id,
-    assessment_tool.id      assessment_tool_id,
-    assessment_tool_mode.id assessment_tool_mode_id,
-    state.id                state_id
-  FROM checkpoint_score
-    INNER JOIN checkpoint ON checkpoint_score.checkpoint_id = checkpoint.id
-    INNER JOIN measurable_element ON checkpoint.measurable_element_id = measurable_element.id
-    INNER JOIN standard ON measurable_element.standard_id = standard.id
-    INNER JOIN area_of_concern ON standard.area_of_concern_id = area_of_concern.id
-    INNER JOIN checklist ON checkpoint.checklist_id = checklist.id AND checkpoint_score.checklist_id = checklist.id
-    INNER JOIN assessment_tool ON checklist.assessment_tool_id = assessment_tool.id
-    INNER JOIN assessment_tool_mode ON assessment_tool.assessment_tool_mode_id = assessment_tool_mode.id
-    INNER JOIN facility_assessment ON assessment_tool.id = facility_assessment.assessment_tool_id AND checkpoint_score.facility_assessment_id = facility_assessment.id
-    INNER JOIN facility ON facility_assessment.facility_id = facility.id
-    INNER JOIN district ON facility.district_id = district.id
-    INNER JOIN state ON district.state_id = state.id AND checklist.state_id = state.id;
 
 CREATE VIEW assessment_denormalised AS
   SELECT
-    facility_assessment.id          facility_assessment,
-    assessment_tool.id              assessment_tool,
-    assessment_tool_mode.id         assessment_type,
-    facility_assessment.series_name series,
-    facility.id                     facility,
-    facility_type.id                facility_type,
-    district.id                     district,
-    state.id                        state
+    facility_assessment.id                              facility_assessment_id,
+    assessment_tool.name                                assessment_tool_name,
+    assessment_tool_mode.name                           program,
+    assessment_type.name                                assessment_type,
+    facility.name                                       facility_name,
+    facility_assessment.facility_name                   non_coded_facility_name,
+    facility_type.name                                  facility_type_name,
+    facility_assessment.series_name                     assessment_number,
+    facility_assessment.start_date                      start_date,
+    facility_assessment.end_date                        end_date,
+    state.name                                          state_name,
+    district.name                                       district_name
   FROM facility_assessment
     INNER JOIN assessment_tool ON facility_assessment.assessment_tool_id = assessment_tool.id
     INNER JOIN assessment_tool_mode ON assessment_tool.assessment_tool_mode_id = assessment_tool_mode.id
+    INNER JOIN assessment_type on assessment_type.id = facility_assessment.assessment_type_id
     INNER JOIN facility ON facility_assessment.facility_id = facility.id
     INNER JOIN district ON facility.district_id = district.id
     INNER JOIN state ON district.state_id = state.id
@@ -128,10 +95,11 @@ CREATE VIEW checklist_score_view AS
     assessment_tool_mode.id                                  AS program,
     fa.id                                                    AS facility_assessment_id,
     fa.assessment_code                                       AS assessment_code,
-    fa.series_name                                           AS assessment_number,
+    to_number(fa.series_name, '9999')                        AS assessment_number,
     facility.id                                              AS facility,
     assessment_type.id                                       AS assessment_type,
     facility_type.id                                         AS facility_type,
+    facility_type.name                                         AS facility_type_name,
     aoc.id                                                   AS area_of_concern,
     format('%s (%s)', aoc.reference, aoc.name)               AS area_of_concern_display,
     cl.id                                                    AS checklist,
@@ -162,13 +130,14 @@ CREATE VIEW area_of_concern_score_view AS
     assessment_tool_mode.id                    AS assessment_type,
     fa.id                                      AS facility_assessment_id,
     fa.assessment_code                         AS assessment_code,
-    fa.series_name                             AS assessment_number,
+    to_number(fa.series_name, '9999')          AS assessment_number,
     facility.id                                AS facility,
     format('%s (%s)', aoc.reference, aoc.name) AS area_of_concern,
     aoc.reference                              AS area_of_concern_reference,
     aocs.score                                 AS score,
     state.id                                   AS state,
-    facility_type.id                           AS facility_type
+    facility_type.id                           AS facility_type,
+    facility_type.name                           AS facility_type_name
   FROM area_of_concern_score aocs
     INNER JOIN area_of_concern aoc ON aoc.id = aocs.area_of_concern_id
     INNER JOIN facility_assessment fa ON aocs.facility_assessment_id = fa.id
@@ -181,17 +150,18 @@ CREATE VIEW area_of_concern_score_view AS
 
 CREATE VIEW standard_score_view AS
   SELECT
-    ss.id                   as id,
+    ss.id                               as id,
     assessment_tool_mode.id AS assessment_type,
     fa.id                   AS facility_assessment_id,
     fa.assessment_code      AS assessment_code,
-    fa.series_name          AS assessment_number,
+    to_number(fa.series_name, '9999')          AS assessment_number,
     facility.id             AS facility,
     s.reference             AS standard,
     s.name                  AS standard_name,
     ss.score                AS score,
     state.id                AS state,
-    facility_type.id        AS facility_type
+    facility_type.id        AS facility_type,
+    facility_type.name        AS facility_type_name
   FROM standard_score ss
     INNER JOIN standard s ON s.id = ss.standard_id
     INNER JOIN facility_assessment fa ON ss.facility_assessment_id = fa.id
