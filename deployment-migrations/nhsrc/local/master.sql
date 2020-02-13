f49e4de2dcf01cb2d220c03f2b7fe159fd7ac713
Vivek Singh <petmongrels@gmail.com>
2020-02-13 15:35:13 +0530

    kerala chc

--- deployment-migrations/nhsrc/local/master.sql
+++ deployment-migrations/nhsrc/local/master.sql
@@ -1,18 +1,20 @@
-\i ./1-1-update-me-kk.sql
-\i ./1-2-update-checkpoint-kk.sql
-\i ./2-update-methods-kk.sql
-\i ./3-1-1-kk-hwc-new.sql
-\i ./3-1-2-kk-phc.sql
-\i ./3-1-3-0-nqas-dh.sql
-\i ./3-1-3-1-nqas-dh.sql
-\i ./3-1-3-2-nqas-dh.sql
-\i ./3-1-3-3-nqas-dh.sql
-\i ./3-1-4-nqas-phc.sql
-\i ./3-1-5-nqas-uphc.sql
-\i ./3-1-7-0-nqas-CHC.sql
-\i ./3-1-7-1-nqas-CHC.sql
-\i ./3-1-7-2-nqas-CHC.sql
-\i ./3-1-7-3-nqas-CHC.sql
-\i ./4-1-1-kerala-nqas-phc.sql
-\i ./4-1-2-1-kerala-nqas-dh.sql
-\i ./4-1-2-2-kerala-nqas-dh-checkpoints.sql
+-- \i ./1-1-update-me-kk.sql
+-- \i ./1-2-update-checkpoint-kk.sql
+-- \i ./2-update-methods-kk.sql
+-- \i ./3-1-1-kk-hwc-new.sql
+-- \i ./3-1-2-kk-phc.sql
+-- \i ./3-1-3-0-nqas-dh.sql
+-- \i ./3-1-3-1-nqas-dh.sql
+-- \i ./3-1-3-2-nqas-dh.sql
+-- \i ./3-1-3-3-nqas-dh.sql
+-- \i ./3-1-4-nqas-phc.sql
+-- \i ./3-1-5-nqas-uphc.sql
+-- \i ./3-1-7-0-nqas-CHC.sql
+-- \i ./3-1-7-1-nqas-CHC.sql
+-- \i ./3-1-7-2-nqas-CHC.sql
+-- \i ./3-1-7-3-nqas-CHC.sql
+-- \i ./4-1-1-kerala-nqas-phc.sql
+-- \i ./4-1-2-1-kerala-nqas-dh.sql
+-- \i ./4-1-2-2-kerala-nqas-dh-checkpoints.sql
+\i ./4-1-3-1-kerala-nqas-chc.sql
+\i ./4-1-3-2-kerala-nqas-chc-checkpoints.sql
