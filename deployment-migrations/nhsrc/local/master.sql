feb2b588cf9222aa7bff01b73d28727638a62e02
Vivek Singh <petmongrels@gmail.com>
2020-02-17 14:15:48 +0530

    .

--- deployment-migrations/nhsrc/local/master.sql
+++ deployment-migrations/nhsrc/local/master.sql
@@ -1,24 +1,24 @@
--- \i ./1-1-update-me-kk.sql
--- \i ./1-2-update-checkpoint-kk.sql
--- \i ./2-update-methods-kk.sql
--- \i ./3-1-1-kk-hwc-new.sql
--- \i ./3-1-2-kk-phc.sql
--- \i ./3-1-3-0-nqas-dh.sql
--- \i ./3-1-3-1-nqas-dh.sql
--- \i ./3-1-3-2-nqas-dh.sql
--- \i ./3-1-3-3-nqas-dh.sql
--- \i ./3-1-4-nqas-phc.sql
--- \i ./3-1-5-nqas-uphc.sql
--- \i ./3-1-7-0-nqas-CHC.sql
--- \i ./3-1-7-1-nqas-CHC.sql
--- \i ./3-1-7-2-nqas-CHC.sql
--- \i ./3-1-7-3-nqas-CHC.sql
--- \i ./4-1-1-kerala-nqas-phc.sql
--- \i ./4-1-2-1-kerala-nqas-dh.sql
--- \i ./4-1-2-2-kerala-nqas-dh-checkpoints.sql
--- \i ./4-1-3-1-kerala-nqas-chc.sql
--- \i ./4-1-3-2-kerala-nqas-chc-checkpoints.sql
--- \i ./4-1-4-1-kerala-nqas-taluka.sql
--- \i ./4-1-4-2-kerala-nqas-taluka-checkpoint.sql
+\i ./1-1-update-me-kk.sql
+\i ./1-2-update-checkpoint-kk.sql
+\i ./2-update-methods-kk.sql
+\i ./3-1-1-kk-hwc-new.sql
+\i ./3-1-2-kk-phc.sql
+\i ./3-1-3-0-nqas-dh.sql
+\i ./3-1-3-1-nqas-dh.sql
+\i ./3-1-3-2-nqas-dh.sql
+\i ./3-1-3-3-nqas-dh.sql
+\i ./3-1-4-nqas-phc.sql
+\i ./3-1-5-nqas-uphc.sql
+\i ./3-1-7-0-nqas-CHC.sql
+\i ./3-1-7-1-nqas-CHC.sql
+\i ./3-1-7-2-nqas-CHC.sql
+\i ./3-1-7-3-nqas-CHC.sql
+\i ./4-1-1-kerala-nqas-phc.sql
+\i ./4-1-2-1-kerala-nqas-dh.sql
+\i ./4-1-2-2-kerala-nqas-dh-checkpoints.sql
+\i ./4-1-3-1-kerala-nqas-chc.sql
+\i ./4-1-3-2-kerala-nqas-chc-checkpoints.sql
+\i ./4-1-4-1-kerala-nqas-taluka.sql
+\i ./4-1-4-2-kerala-nqas-taluka-checkpoint.sql
 -- \i ./4-2-1-up-nqas-chc.sql
-\i ./4-2-2-up-nqas-dhmale.sql
+-- \i ./4-2-2-up-nqas-dhmale.sql
