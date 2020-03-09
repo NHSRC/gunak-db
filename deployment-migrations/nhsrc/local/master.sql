4f1fb467dd4b6b476c85b079004b50464e38538d
Vivek Singh <petmongrels@gmail.com>
2020-03-09 14:24:40 +0530

    fixes

--- deployment-migrations/nhsrc/local/master.sql
+++ deployment-migrations/nhsrc/local/master.sql
@@ -1,32 +1,39 @@
--- \i ./1-1-update-me-kk.sql
--- \i ./1-2-update-checkpoint-kk.sql
--- \i ./2-update-methods-kk.sql
--- \i ./3-1-1-kk-hwc-new.sql
--- \i ./3-1-2-kk-phc.sql
--- \i ./3-1-3-0-nqas-dh.sql
--- \i ./3-1-3-1-nqas-dh.sql
--- \i ./3-1-3-2-nqas-dh.sql
--- \i ./3-1-3-3-nqas-dh.sql
--- \i ./3-1-4-nqas-phc.sql
--- \i ./3-1-5-nqas-uphc.sql
--- \i ./3-1-7-0-nqas-CHC.sql
--- \i ./3-1-7-1-nqas-CHC.sql
--- \i ./3-1-7-2-nqas-CHC.sql
--- \i ./3-1-7-3-nqas-CHC.sql
--- \i ./4-1-1-kerala-nqas-phc.sql
--- \i ./4-1-2-1-kerala-nqas-dh.sql
--- \i ./4-1-2-2-kerala-nqas-dh-checkpoints.sql
--- \i ./4-1-3-1-kerala-nqas-chc.sql
--- \i ./4-1-3-2-kerala-nqas-chc-checkpoints.sql
--- \i ./4-1-4-1-kerala-nqas-taluka.sql
--- \i ./4-1-4-2-kerala-nqas-taluka-checkpoint.sql
--- \i ./4-2-1-up-nqas-chc.sql
--- \i ./4-2-2-up-nqas-dhmale.sql
--- \i ./4-2-3-up-nqas-dhfemale.sql
--- \i ./4-3-1-tn-nqas-phc.sql
--- \i ./4-3-2-tn-nqas-dh.sql
--- \i ./4-3-3-tn-nqas-chc.sql
--- \i ./4-3-4-tn-nqas-sdh.sql
--- \i ./4-4-1-meghalaya-nqas-chc.sql
+\i ./1-1-update-me-kk.sql
+\i ./1-2-update-checkpoint-kk.sql
+\i ./2-update-methods-kk.sql
+\i ./3-1-1-kk-hwc-new.sql
+\i ./3-1-2-kk-phc.sql
+\i ./3-1-3-0-nqas-dh.sql
+\i ./3-1-3-1-nqas-dh.sql
+\i ./3-1-3-2-nqas-dh.sql
+\i ./3-1-3-3-nqas-dh.sql
+\i ./3-1-4-nqas-phc.sql
+\i ./3-1-5-nqas-uphc.sql
+\i ./3-1-7-0-nqas-CHC.sql
+\i ./3-1-7-1-nqas-CHC.sql
+\i ./3-1-7-2-nqas-CHC.sql
+\i ./3-1-7-3-nqas-CHC.sql
+\i ./4-1-1-kerala-nqas-phc.sql
+\i ./4-1-2-1-kerala-nqas-dh.sql
+\i ./4-1-2-2-kerala-nqas-dh-checkpoints.sql
+\i ./4-1-3-1-kerala-nqas-chc.sql
+\i ./4-1-3-2-kerala-nqas-chc-checkpoints.sql
+\i ./4-1-4-1-kerala-nqas-taluka.sql
+\i ./4-1-4-2-kerala-nqas-taluka-checkpoint.sql
+\i ./4-2-1-up-nqas-chc.sql
+\i ./4-2-2-up-nqas-dhmale.sql
+\i ./4-2-3-up-nqas-dhfemale.sql
+\i ./4-3-1-tn-nqas-phc.sql
+\i ./4-3-2-tn-nqas-dh.sql
+\i ./4-3-3-tn-nqas-chc.sql
+\i ./4-3-4-tn-nqas-sdh.sql
 
--- Change server to provide AOC, Standard, ME which are state specific or generic and not excluded
+
+-- Change server code to provide AOC, Standard, ME which are state specific or generic and not excluded
+
+
+
+
+
+
+
