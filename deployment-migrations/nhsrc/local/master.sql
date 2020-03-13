20285be5b6a40dfc59264fbb005909c47000e130
Vivek Singh <petmongrels@gmail.com>
2020-03-13 11:47:27 +0530

    hp checklists

--- deployment-migrations/nhsrc/local/master.sql
+++ deployment-migrations/nhsrc/local/master.sql
@@ -30,6 +30,9 @@
 \i ./4-3-4-tn-nqas-sdh.sql
 \i ./4-4-1-megh-nqas-chc.sql
 \i ./4-4-2-megh-nqas-mch.sql
+\i ./4-5-1-hp-nqas-30-50-bedded.sql
+\i ./4-5-2-hp-nqas-100-199-bed.sql
+\i ./4-5-3-hp-nqas-200-bed.sql
 
 update assessment_tool_mode set name = 'NQAS' where name = 'nqas';
 
