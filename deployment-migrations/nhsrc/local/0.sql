6a9eb76df66b14873bf390679e6c65cfca4ac957
Vivek Singh <petmongrels@gmail.com>
2020-03-18 22:50:39 +0530

    multiple states

--- deployment-migrations/nhsrc/local/0.sql
+++ deployment-migrations/nhsrc/local/0.sql
@@ -1,1 +1,12 @@
 update assessment_tool_mode set name = 'nqas' where name = 'NQAS';
+
+delete from assessment_tool_mode where name = 'H &W C';
+delete from assessment_tool where name in ('H & WC', 'H& WC');
+
+delete from assessment_tool_checklist
+  where id in
+      (select assessment_tool_checklist.id from assessment_tool_checklist
+      join checklist c2 on assessment_tool_checklist.checklist_id = c2.id
+      where c2.name in ('District Women & Child Hospital', 'District Women & Child Hospital '));
+
+delete from checklist where name in ('District Women & Child Hospital', 'District Women & Child Hospital ');
