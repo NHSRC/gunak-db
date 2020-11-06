06a4222e58cf6b8fb4169e054aeddcfe26111bbf
Vivek Singh <petmongrels@gmail.com>
2020-11-06 17:54:50 +0530

    nqas levels

--- deployment-migrations/nhsrc/qa/nqas-levels.sql
+++ deployment-migrations/nhsrc/qa/nqas-levels.sql
@@ -1,40 +1,47 @@
--- Universal
-update assessment_tool set level = 50 where name = 'District Hospital (DH)' and state_id is null;
-update assessment_tool set level = 40 where name = 'Community Health Center (CHC)' and state_id is null;
-update assessment_tool set level = 20 where name = 'Primary Health Center (PHC)' and state_id is null;
-update assessment_tool set level = 20 where name = 'Urban Primary Health Center (UPHC)' and state_id is null;
+-- Universal NQAS
+update assessment_tool set sort_order = 50, last_modified_date = current_timestamp where name = 'District Hospital (DH)' and state_id is null;
+update assessment_tool set sort_order = 40, last_modified_date = current_timestamp where name = 'Community Health Center (CHC)' and state_id is null;
+update assessment_tool set sort_order = 20, last_modified_date = current_timestamp where name = 'Primary Health Center (PHC)' and state_id is null;
+update assessment_tool set sort_order = 20, last_modified_date = current_timestamp where name = 'Urban Primary Health Center (UPHC)' and state_id is null;
 -- Kar
-update assessment_tool set level = 30 where name = 'Taluka Level (Karnataka)' and state_id is null;
+update assessment_tool set sort_order = 30, last_modified_date = current_timestamp where name ='Taluka sort_order (Karnataka)';
 -- Odisha
-update assessment_tool set level = 50 where name = 'District Hospital (Odisha)' and state_id is null;
+update assessment_tool set sort_order = 50, last_modified_date = current_timestamp where name ='District Hospital (Odisha)';
 -- Uttar Pradesh
-update assessment_tool set level = 40 where name = 'Community Health Center (UP CHC)' and state_id is null;
-update assessment_tool set level = 50 where name = 'District Male Hospital (UP)' and state_id is null;
-update assessment_tool set level = 50 where name = 'District Women Hospital (UP)' and state_id is null;
+update assessment_tool set sort_order = 40, last_modified_date = current_timestamp where name ='Community Health Center (UP CHC)';
+update assessment_tool set sort_order = 50, last_modified_date = current_timestamp where name ='District Male Hospital (UP)';
+update assessment_tool set sort_order = 50, last_modified_date = current_timestamp where name ='District Women Hospital (UP)';
 -- Tamil Nadu
-update assessment_tool set level = 20 where name = 'Primary Health Center (TN)' and state_id is null;
-update assessment_tool set level = 50 where name = 'District Hospital (TN)' and state_id is null;
-update assessment_tool set level = 40 where name = 'Community Health Center (TN)' and state_id is null;
-update assessment_tool set level = 45 where name = 'Sub District Hospital (SDH)' and state_id is null;
+update assessment_tool set sort_order = 20, last_modified_date = current_timestamp where name ='Primary Health Center (TN)';
+update assessment_tool set sort_order = 50, last_modified_date = current_timestamp where name ='District Hospital (TN)';
+update assessment_tool set sort_order = 40, last_modified_date = current_timestamp where name ='Community Health Center (TN)';
+update assessment_tool set sort_order = 45, last_modified_date = current_timestamp where name ='Sub District Hospital (SDH)';
 -- Himachal Pradesh
-update assessment_tool set level = 30 where name = '30-50 Bedded' and state_id is null;
-update assessment_tool set level = 40 where name = '100-199 Bedded (HP)' and state_id is null;
-update assessment_tool set level = 50 where name = '200 Bedded (HP)' and state_id is null;
+update assessment_tool set sort_order = 30, last_modified_date = current_timestamp where name ='30-50 Bedded';
+update assessment_tool set sort_order = 40, last_modified_date = current_timestamp where name ='100-199 Bedded (HP)';
+update assessment_tool set sort_order = 50, last_modified_date = current_timestamp where name ='200 Bedded (HP)';
 -- Jammu and Kashmir
-update assessment_tool set level = 50 where name = 'District Hospital (J and K)' and state_id is null;
+update assessment_tool set sort_order = 50, last_modified_date = current_timestamp where name ='District Hospital (J and K)';
 -- Madhya Pradesh
-update assessment_tool set level = 50 where name = 'District Hospital (MP)' and state_id is null;
-update assessment_tool set level = 45 where name = 'Women Hospital (MP)' and state_id is null;
+update assessment_tool set sort_order = 50, last_modified_date = current_timestamp where name ='District Hospital (MP)';
+update assessment_tool set sort_order = 45, last_modified_date = current_timestamp where name ='Women Hospital (MP)';
 -- Punjab
-update assessment_tool set level = 50 where name = 'District Hospital (Punjab)' and state_id is null;
-update assessment_tool set level = 45 where name = 'SDH (Punjab)' and state_id is null;
+update assessment_tool set sort_order = 50, last_modified_date = current_timestamp where name ='District Hospital (Punjab)';
+update assessment_tool set sort_order = 45, last_modified_date = current_timestamp where name ='SDH (Punjab)';
 -- Telangana
-update assessment_tool set level = 20 where name = 'UPHC (Telangana)' and state_id is null;
+update assessment_tool set sort_order = 20, last_modified_date = current_timestamp where name ='UPHC (Telangana)';
 -- Kerala
-update assessment_tool set level = 20 where name = 'Primary Health Centre (Kerala)' and state_id is null;
-update assessment_tool set level = 45 where name = 'District Women and Child Hospital' and state_id is null;
-update assessment_tool set level = 40 where name = 'Community Health Center (Kerala)' and state_id is null;
-update assessment_tool set level = 35 where name = 'Taluka Level (Kerala)' and state_id is null;
+update assessment_tool set sort_order = 20, last_modified_date = current_timestamp where name ='Primary Health Centre (Kerala)';
+update assessment_tool set sort_order = 45, last_modified_date = current_timestamp where name ='District Women and Child Hospital';
+update assessment_tool set sort_order = 40, last_modified_date = current_timestamp where name ='Community Health Center (Kerala)';
+update assessment_tool set sort_order = 35, last_modified_date = current_timestamp where name ='Taluka sort_order (Kerala)';
 -- Meghalaya
-update assessment_tool set level = 45 where name = 'District MCH (Meghalaya)' and state_id is null;
-update assessment_tool set level = 40 where name = 'Community Health Center (Meghalaya)' and state_id is null;
+update assessment_tool set sort_order = 45, last_modified_date = current_timestamp where name ='District MCH (Meghalaya)';
+update assessment_tool set sort_order = 40, last_modified_date = current_timestamp where name ='Community Health Center (Meghalaya)';
+
+-- Universal Kayakalp
+update assessment_tool set sort_order = 50, last_modified_date = current_timestamp where name ='DH, SDH and CHC';
+update assessment_tool set sort_order = 20, last_modified_date = current_timestamp where name ='PHC with bed';
+update assessment_tool set sort_order = 20, last_modified_date = current_timestamp where name ='PHC without bed';
+update assessment_tool set sort_order = 10, last_modified_date = current_timestamp where name ='Health and Wellness Centre';
+
