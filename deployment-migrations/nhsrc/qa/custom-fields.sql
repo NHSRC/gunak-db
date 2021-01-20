2825a8b83b84133b5afbc1f7b2672e8bb131bf6e
Vivek Singh <petmongrels@gmail.com>
2021-01-20 15:33:26 +0530

    .

--- deployment-migrations/nhsrc/qa/custom-fields.sql
+++ deployment-migrations/nhsrc/qa/custom-fields.sql
@@ -1,4 +1,4 @@
 insert into assessment_metadata (name, data_type, mandatory) VALUES ('Assessor''s designation', 'String', true);
 insert into assessment_metadata (name, data_type, validation_regex, mandatory) VALUES ('Assessor''s phone number (10 digits)', 'String', '^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$', true);
 
-update assessment_metadata set validation_regex = '^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$', last_modified_date = current_timestamp where name = 'Assessor''s phone number';
+update assessment_metadata set validation_regex = '^[0-9]{10}', last_modified_date = current_timestamp where name = 'Assessor''s phone number (10 digits)';
