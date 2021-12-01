942404c4f7be7537995513b4e3654bea6d6fc595
Vivek Singh <petmongrels@gmail.com>
2021-12-01 11:16:33 +0530

    assessment types

--- deployment-migrations/nhsrc/local/musqan.sql
+++ deployment-migrations/nhsrc/local/musqan.sql
@@ -3,3 +3,8 @@ insert into assessment_tool (name, type, assessment_tool_mode_id, sort_order)
     VALUES ('Annexure A - DH', 'INDICATOR', (select id from assessment_tool_mode where name = 'MusQan'), 50);
 insert into assessment_tool (name, type, assessment_tool_mode_id, sort_order)
     VALUES ('Annexure A - CHC', 'INDICATOR', (select id from assessment_tool_mode where name = 'MusQan'), 40);
+
+insert into assessment_type (name, assessment_tool_mode_id) values ('External', (select id from assessment_tool_mode where name = 'MusQan'));
+insert into assessment_type (name, assessment_tool_mode_id) values ('Internal', (select id from assessment_tool_mode where name = 'MusQan'));
+insert into assessment_type (name, assessment_tool_mode_id) values ('State', (select id from assessment_tool_mode where name = 'MusQan'));
+insert into assessment_type (name, assessment_tool_mode_id) values ('Surveillance', (select id from assessment_tool_mode where name = 'MusQan'));
